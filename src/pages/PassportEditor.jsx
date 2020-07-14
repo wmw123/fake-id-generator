1e102e6b9989f2ba09436f5c0b550dfe55d4facb
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-14 15:57:40 +0200

    Create selector for countries

--- src/pages/PassportEditor.jsx
+++ src/pages/PassportEditor.jsx
@@ -3,10 +3,25 @@ import './PassportEditor.css';
 import image from '../img/img_passport.jpeg';
 
 export default function PassportEditor() {
+  const countries = ['kingdom of the netherlands', 'belgium', 'germany'];
+
+  const renderCountries = () => {
+    return (
+      <select>
+        {countries.map((country, i) => (
+          <option key={i} value={country}>
+            {country.toUpperCase()}
+          </option>
+        ))}
+      </select>
+    );
+  };
+
   return (
-    <div class="passport-container">
-      <div class="passport__page--2">
-        <img src={image} alt="image passport" />
+    <div className="passport-container">
+      <div className="passport__page--2">
+        <div className="header">{renderCountries()}</div>
+        <img className="picture" src={image} alt="passport" />
         passport
       </div>
     </div>
