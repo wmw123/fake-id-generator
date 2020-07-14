ddc938623a17bbbd8cdb32564966fe295854d96d
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-14 15:46:31 +0200

    Import image and set up page--2

--- src/pages/PassportEditor.jsx
+++ src/pages/PassportEditor.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './PassportEditor.css';
+import image from '../img/img_passport.jpeg';
 
 export default function PassportEditor() {
-  return <div class="passport-container">passport</div>;
+  return (
+    <div class="passport-container">
+      <div class="passport__page--2">
+        <img src={image} alt="image passport" />
+        passport
+      </div>
+    </div>
+  );
 }
