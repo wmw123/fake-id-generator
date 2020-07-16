93d426898a9e3bec51cabd3d32f1697c9006a279
wmw123 <wmwaterbolk@gmail.com>
2020-07-16 10:52:29 +0200

    navigation added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import WebcamCapture from "../components/WebcamCapture";
+import { useHistory } from "react-router-dom";
 
 export default function Home() {
+  const history = useHistory();
   const takePhoto = (e) => {
     e.preventDefault();
 
@@ -24,6 +26,14 @@ export default function Home() {
     <>
       <div>
         <h1>Homepage</h1>
+      </div>
+      <div>
+        <button onClick={() => history.push("/passporteditor")}>
+          Check out your passport
+        </button>
+        <button onClick={() => history.push("/photoeditor")}>
+          Edit your picture
+        </button>
       </div>
       <div>
         <div>{button}</div>
